550248c601ae5d166b5431a02f0a9831b61a2460
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-10 15:40:49 +0100

    image rendered

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      image: `<img src='https://unsplash.it/600/300?image=151' />`
     }
   })
